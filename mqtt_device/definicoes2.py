99404eabad66b615c3c35e588823bd5da1fcf416
evandroafonso <evandroafonsorodrigues@gmail.com>
2022-09-25 00:00:05 -0300

    add devices2

---
+++ mqtt_device/definicoes2.py
@@ -0,0 +1,5 @@
+user = '26c44a20-3c4d-11ed-bf0a-bb4ba43bd3f6'
+password = '6176007f6e8f2dfe36c466c2937dd78ad30cb315'
+client_id = 'dabc7af0-3c7c-11ed-bf0a-bb4ba43bd3f6'
+server = 'mqtt.mydevices.com'
+port = 1883
--- mqtt_device/dispositivo2.py
+++ mqtt_device/dispositivo2.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import time
 from hal1 import temperaturaDefinida, statusAquecedor, statusRele
 import constantes
-from definicoes1 import user, password, client_id, server, port
+from definicoes2 import user, password, client_id, server, port
 
 
 def mensagem(client, userdata, msg):
@@ -16,14 +16,14 @@ client.username_pw_set(user, password)
 client.connect(server, port)
 
 client.on_message = mensagem
-client.subscribe(f'v1/{user}/things/{client_id}/cmd/2')
+client.subscribe(f'v1/{user}/things/{client_id}/cmd/5')
 client.loop_start()
 
 while True:
-    client.publish(f'v1/{user}/things/{client_id}/data/0', temperaturaDefinida())
+    client.publish(f'v1/{user}/things/{client_id}/data/3', temperaturaDefinida())
     statusRele();
     if(constantes.releStatus == True):
-        client.publish(f'v1/{user}/things/{client_id}/data/1', '1')
+        client.publish(f'v1/{user}/things/{client_id}/data/4', '1')
     else:
-        client.publish(f'v1/{user}/things/{client_id}/data/1', '0')
+        client.publish(f'v1/{user}/things/{client_id}/data/4', '0')
     time.sleep(5)
